83680bcbb30a0a163b87d47c93523c0a123a259e
lnardon <lucaslnardon@gmail.com>
2021-02-09 23:29:15 -0300

    🐛 Update custom hook

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -3,7 +3,9 @@ import { useHistory } from "react-router-dom";
 export default function useValidation() {
   const history = useHistory();
   if (!localStorage.getItem("@jwt")) {
-    history.push("/");
+    if (!(window.location.pathname === "/signup")) {
+      history.push("/");
+    }
   } else {
     if (window.location.pathname !== "/dashboard") {
       history.push("/dashboard");
